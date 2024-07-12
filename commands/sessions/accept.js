f2a241bbc4fafa168886b4a9463ee7ae93fb5662
Cmb <71194682+29cmb@users.noreply.github.com>
2024-07-12 17:51:06 -0400

    Accept and deny (then I'll deploy on a server

--- commands/sessions/accept.js
+++ commands/sessions/accept.js
@@ -7,7 +7,19 @@ module.exports = {
 	async execute(interaction) {
 		await db.client.connect()
 		if(await db.collections.reviewers.findOne({ uid: interaction.user.id })){
-            
+            if(!interaction.channel.isThread()) return interaction.reply("This command can only be ran in threads!");
+			const thread = await db.collections.threads.findOne({ tid: interaction.channel.id, active: false, reviewed: false })
+			if(!thread) return interaction.reply("This is not a reviewable session!")
+
+			interaction.channel.send("Congrats! This session has been approved!")
+			db.collections.threads.updateOne({ tid: interaction.channel.id, active: false, reviewed: false }, {"$set": {
+				uid: thread.uid,
+				tid: thread.tid,
+				active: false,
+				reviewed: true,
+				accepted: true,
+				time: (thread.endTime - thread.startTime) / 1000
+			}})
         }
 	},
 };
