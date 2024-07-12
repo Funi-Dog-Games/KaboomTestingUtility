d374531523459bda084d40dc6ab7774119d65c9e
Cmb <71194682+29cmb@users.noreply.github.com>
2024-07-12 09:41:55 -0400

    Added start command

---
+++ commands/sessions/start.js
@@ -0,0 +1,36 @@
+const { SlashCommandBuilder, ThreadAutoArchiveDuration, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('start')
+		.setDescription('Start a session'),
+	async execute(interaction) {
+		await interaction.deferReply();
+        const sentMsg = await interaction.editReply("Session Started!");
+
+        const thread = await sentMsg.startThread({
+            name: `${interaction.user.username}'s Session`,
+            autoArchiveDuration: ThreadAutoArchiveDuration.OneHour,
+            reason: `${interaction.user.username} has been inactive for 1 hour!`
+        });
+
+        const endButton = new ButtonBuilder()
+        .setCustomId("end")
+        .setLabel("End")
+        .setStyle(ButtonStyle.Danger)
+
+        const actionRow = new ActionRowBuilder()
+        .addComponents([endButton])
+
+        const reply = await thread.send({ content: `<@${interaction.user.id}>, while you work, post images or summaries of what you're doing!`, components: [actionRow] })
+        const buttonClick = await reply.awaitMessageComponent()
+        if(buttonClick.user.id == interaction.user.id){
+            if(buttonClick.customId == "end"){
+                await buttonClick.update({ content: "Session over! Great job!", components: [] })
+                await thread.setLocked(true)
+            }
+        }else {
+            buttonClick.reply({ content: `These buttons aren't for you!`, ephemeral: true });
+        }
+	},
+};
