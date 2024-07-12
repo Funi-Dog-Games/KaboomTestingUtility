06d2aaf7e964e7113866e47c6de2045d0ab3f431
Cmb <71194682+29cmb@users.noreply.github.com>
2024-07-12 17:01:18 -0400

    Add some checks

--- commands/sessions/review.js
+++ commands/sessions/review.js
@@ -13,7 +13,13 @@ module.exports = {
                 {$sample: {size: 1}}
             ]).toArray()
             await db.client.close()
-            if(session.length == 0){ return interaction.reply("There are no sessions to review!") }
+            if(session.length == 0) return interaction.reply("There are no sessions to review!")
+            
+            const channel = await interaction.client.channels.cache.get(session[0].channel)
+            if(!channel) return interaction.reply("Could not start a review since the channel the thread was made on does not exist.")
+            const thread = channel.threads.cache.find(id => id.id == session[0].tid)
+            if(!thread) return interaction.reply("Could not start a review since the thread has been deleted or archived.")
+                
             interaction.reply(`Found a session! Your review will be on <#${session[0].tid}>`)
         } else {
             await db.client.close()
