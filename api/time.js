5b0960f4b68c3a3d1056116b124b989d8b806c6d
Cmb <71194682+29cmb@users.noreply.github.com>
2024-07-13 14:03:23 -0400

    Time API

---
+++ api/time.js
@@ -0,0 +1,35 @@
+const db = require("../modules/db.js")
+module.exports = (app) => {
+    app.post("/time", async (req, res) => {
+        const { body } = req
+        if(!body || !body.uid) return res.status(404).json({ success: false, message: "UID not provided" });
+
+        await db.client.connect()
+        var data = await db.collections.threads.find({
+            uid: body.uid,
+            time: {
+                $exists: 1
+            }
+        }, {
+            time: 1
+        }).toArray()
+
+        if(!data) return res.status(404).json({ success: false, message: "User does not have any approved sessions" })
+
+        var s = 0
+
+        data.forEach((entry) => {
+            if(entry.time){
+                s = s + entry.time
+            }
+        })
+
+        const minutes = Math.floor(s / 60).toString().padStart(2, '0');
+        const seconds = Math.floor(s % 60).toString().padStart(2, '0');
+        return res.status(200).json({ success: true, data: { minutes, seconds } })
+    })
+    return {
+        method: "POST",
+        route: "/time"
+    }
+}
