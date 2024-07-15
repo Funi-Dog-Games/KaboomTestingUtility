a0a5f866a82815a9eec4d35f9f739a1e9263c58d
Cmb <71194682+29cmb@users.noreply.github.com>
2024-07-15 12:45:38 -0400

    Addthe ability to provide a custom minutes value

--- commands/sessions/accept.js
+++ commands/sessions/accept.js
@@ -3,13 +3,18 @@ const db = require("../../modules/db.js")
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('accept')
-		.setDescription('Accept the current thread (reviewers only)'),
+		.setDescription('Accept the current thread (reviewers only)')
+		.addStringOption(o => 
+			o.setName("minutes").setDescription("How many minutes to give").setRequired(false)
+		),
 	async execute(interaction) {
 		await db.client.connect()
 		if(await db.collections.reviewers.findOne({ uid: interaction.user.id })){
             if(!interaction.channel.isThread()) return interaction.reply("This command can only be ran in threads!");
 			const thread = await db.collections.threads.findOne({ tid: interaction.channel.id, active: false, reviewed: false })
 			if(!thread) return interaction.reply("This is not a reviewable session!")
+
+			const minutes = interaction.options.getString("minutes") || (thread.endTime - thread.startTime) / 1000
 
 			interaction.channel.send("Congrats! This session has been approved!")
 			await db.collections.threads.updateOne({ tid: interaction.channel.id, active: false, reviewed: false }, {"$set": {
@@ -18,19 +23,19 @@ module.exports = {
 				active: false,
 				reviewed: true,
 				accepted: true,
-				time: (thread.endTime - thread.startTime) / 1000
+				time: minutes
 			}})
 
 			const user = await db.collections.users.findOne({ uid: interaction.user.id })
 			if(user){
 				await db.collections.users.updateOne({ uid: interaction.user.id }, {"$set": {
 					uid: interaction.user.id,
-					quota: user.quota + ((thread.endTime - thread.startTime) / 1000)
+					quota: user.quota + minutes
 				}})
 			} else {
 				await db.collections.users.insertOne({
 					uid: interaction.user.id,
-					quota: (thread.endTime - thread.startTime) / 1000
+					quota: minutes
 				})
 			}
 
