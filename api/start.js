fefdf5bc2aed2d854b051689d2cb7c777e8402dc
Cmb <71194682+29cmb@users.noreply.github.com>
2024-07-21 12:54:04 -0400

    It FINALLY works

--- api/start.js
+++ api/start.js
@@ -3,19 +3,18 @@ const db = require("../modules/db")
 module.exports = (app) => {
     app.post("/start", async (req, res) => {
         const { body } = req
-        console.log(body)
         if(!body || body.ruid == undefined) return res.status(400).json({ success: false, message: "RUID not provided" })
         await db.client.connect()   
-        const user = await db.collections.users.findOne({ ruid: body.ruid })
+        const user = await db.collections.users.findOne({ ruid: parseInt(body.ruid) })
         await db.client.close()
         if(!user) { 
             console.log("Request failed on registration check") 
             return res.status(404).json({ success: false, message: "User not found or not registered" }) 
         }
 
-        const d_user = app.client.users.cache.find(id => id.id === user.uid)
-        if(!d_user){ 
-            console.log("Request failed on d_user check")
+        const d_user = await app.client.users.cache.find(id => id.id === user.uid)
+        if(d_user == undefined){ 
+            console.log(`Request failed on d_user check, uid was ${user.uid}`)
             return res.status(404).json({ success: false, message: "Could not find user"}) 
         }
         
