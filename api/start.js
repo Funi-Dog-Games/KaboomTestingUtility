afa8a8ba5d1d4fdfd2a4141d0aecfeaa9dfe91c9
Cmb <71194682+29cmb@users.noreply.github.com>
2024-07-21 12:41:06 -0400

    I am stupid :(

--- api/start.js
+++ api/start.js
@@ -2,14 +2,22 @@ const db = require("../modules/db")
 
 module.exports = (app) => {
     app.post("/start", async (req, res) => {
-        const { body } = req.body
-        if(!body || !body.ruid) return res.status(400).json({ success: false, message: "RUID not provided" })
+        const { body } = req
+        console.log(body)
+        if(!body || body.ruid == undefined) return res.status(400).json({ success: false, message: "RUID not provided" })
         await db.client.connect()   
         const user = await db.collections.users.findOne({ ruid: body.ruid })
         await db.client.close()
-        if(!user) return res.status(404).json({ success: false, message: "User not found or not registered" })
+        if(!user) { 
+            console.log("Request failed on registration check") 
+            return res.status(404).json({ success: false, message: "User not found or not registered" }) 
+        }
+
         const d_user = app.client.users.cache.find(id => id.id === user.uid)
-        if(!d_user) return res.status(404).json({ success: false, message: "Could not find user"})
+        if(!d_user){ 
+            console.log("Request failed on d_user check")
+            return res.status(404).json({ success: false, message: "Could not find user"}) 
+        }
         
         d_user.send("I see you've started playing! How about you start a session using `/start`!")
     })
